7b1269cef4af99ce0d8fd97bff9401b892249002
Alessandro Mencarini <amencarini@housetrip.com>
2013-09-27 14:29:20 +0100

    weird first implementation

--- yarp/cache/s3.rb
+++ yarp/cache/s3.rb
@@ -3,6 +3,7 @@ require 'yarp/logger'
 require 'fog'
 require 'pry'
 
+
 module Yarp::Cache
   # A cache store implementation which stores everything on the filesystem.
   #
@@ -13,19 +14,55 @@ module Yarp::Cache
 
 
     def get(key)
-      puts key
-      _directory.files.get(key)
+      #_directory.files.get(key)
+      _read(key)
     end
 
 
     def fetch(key, ttl=nil) # Check if TTL can be setup
       value = get(key) and return value
       value = yield
-      _upload(key, value, ttl)
+      #_upload(key, value, ttl)
+      _save(key, value, ttl)
     end
 
 
     private
+
+
+      def _save(key, value, ttl)
+        # Check if we're dealing with a binary or not
+        if value.last.encoding.to_s == "ASCII-8BIT"
+          file = ::File.open("files/#{key}.gz", 'wb')
+          file.write(value.last)
+          file.close
+        end
+
+        value_for_saving = value.
+
+        file = ::File.open("files/#{key}", 'w')
+        file.write(Marshal.dump(value))
+        file.close
+
+        value
+      end
+
+
+      def _read(key)
+        if ::File.exist?("files/#{key}")
+          file = ::File.open("files/#{key}", 'r')
+          value = Marshal.load(file.read)
+
+          if ::File.exist?("files/#{key}.gz")
+            file = ::File.open("files/#{key}.gz", 'rb')
+            value.last.replace(file.read)
+            file.close
+          end
+
+
+          value
+        end
+      end
 
 
       def _connection
